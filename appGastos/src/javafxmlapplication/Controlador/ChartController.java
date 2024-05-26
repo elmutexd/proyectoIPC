aee381e6ddc798e92ba1efa84e7e61b1f5b8860e
JulianC <julian.cussianovich@gmail.com>
2024-05-26 12:40:15 +0200

    cosa

--- appGastos/src/javafxmlapplication/Controlador/ChartController.java
+++ appGastos/src/javafxmlapplication/Controlador/ChartController.java
@@ -93,47 +93,36 @@ public class ChartController implements Initializable {
                             break;
                             case FEBRUARY:
                                 contMes[1] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case MARCH:
                                 contMes[2] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case APRIL:
                                 contMes[3] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case MAY:
                                 contMes[4] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case JUNE:
                                 contMes[5] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case JULY:
                                 contMes[6] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case AUGUST:
                                 contMes[7] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case SEPTEMBER:
                                 contMes[8] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case OCTOBER:
                                 contMes[9] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case NOVEMBER:
                                 contMes[10] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                             case DECEMBER:
                                 contMes[11] += carg.get(j).getCost();
-                                carg.remove(j);
                             break;
                         } 
                     }
